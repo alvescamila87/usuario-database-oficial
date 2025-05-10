c88bdecbeb0531026d6fb1db4e33d449b0d309d4
Camila <alvescamila87@gmail.com>
2025-05-10 19:38:44 -0300

    feat: contact

---
+++ src/main/java/com/senai/usuario_database_oficial/models/EnderecoModel.java
@@ -0,0 +1,48 @@
+package com.senai.usuario_database_oficial.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "endereco")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EnderecoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "cep", nullable = false)
+    private String cep;
+
+    @Column(name = "logradouro", nullable = false)
+    private String logradouro;
+
+    @Column(name = "numero")
+    private String numero;
+
+    @Column(name = "complemento")
+    private String complemento;
+
+    @Column(name = "bairro", nullable = false)
+    private String bairro;
+
+    @Column(name = "cidade", nullable = false)
+    private String cidade;
+
+    @Column(name = "uf", nullable = false)
+    private String uf;
+
+    @OneToMany(mappedBy = "endereco")
+    private List<ContatoModel> listaContatos = new ArrayList<>();
+}
