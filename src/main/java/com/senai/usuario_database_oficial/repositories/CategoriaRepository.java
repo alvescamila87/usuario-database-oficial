a28a898885bfa4dbd711119c4ceade07e513b2ff
CAMILA ALVES <alvescamila87@gmail.com>
2025-05-12 19:55:44 -0300

    feat: add categoria service

--- src/main/java/com/senai/usuario_database_oficial/repositories/CategoriaRepository.java
+++ src/main/java/com/senai/usuario_database_oficial/repositories/CategoriaRepository.java
@@ -4,6 +4,10 @@ import com.senai.usuario_database_oficial.models.CategoriaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
+
+    Optional<CategoriaModel> findByNome(String nome);
 }
--- src/main/java/com/senai/usuario_database_oficial/services/CategoriaService.java
+++ src/main/java/com/senai/usuario_database_oficial/services/CategoriaService.java
@@ -1,13 +1,17 @@
 package com.senai.usuario_database_oficial.services;
 
 import com.senai.usuario_database_oficial.dtos.categoria.CategoriaListaDTO;
+import com.senai.usuario_database_oficial.dtos.categoria.CategoriaRequestDTO;
+import com.senai.usuario_database_oficial.exceptions.InvalidOperationException;
 import com.senai.usuario_database_oficial.models.CategoriaModel;
 import com.senai.usuario_database_oficial.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoriaService {
@@ -30,6 +34,30 @@ public class CategoriaService {
         }
 
         return listaCategoriaDTO;
+    }
 
+    public Boolean cadastrarCategoria(CategoriaRequestDTO categoriaRequestDTO) {
+
+        Boolean resultado = validarDupliciadadeNome(categoriaRequestDTO.getNome());
+
+        if(resultado) {
+            throw new InvalidOperationException("Nome de categoria já cadastrado.");
+        }
+
+        CategoriaModel categoriaModel = new CategoriaModel();
+        categoriaModel.setNome(categoriaRequestDTO.getNome());
+        repository.save(categoriaModel);
+
+        return true;
+    }
+
+    protected Boolean validarDupliciadadeNome(String nome){
+        Optional<CategoriaModel> categoriaModel = repository.findByNome(nome);
+
+        if(categoriaModel.isPresent()) {
+            return true;
+        }
+
+        return false;
     }
 }
