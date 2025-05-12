4edff7682f53ca1a2ec226b4e0b88935107a386e
CAMILA ALVES <alvescamila87@gmail.com>
2025-05-12 19:15:09 -0300

    feat: create categoria model

---
+++ src/main/java/com/senai/usuario_database_oficial/models/CategoriaModel.java
@@ -0,0 +1,23 @@
+package com.senai.usuario_database_oficial.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categoria")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoriaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false, unique = true)
+    private String nome;
+}
