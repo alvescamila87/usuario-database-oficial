8bd1c2c44d22d2d742c87a09e93c0abd27258433
Camila <alvescamila87@gmail.com>
2025-05-17 18:27:41 -0300

    feat: session control

---
+++ src/main/java/com/senai/usuario_database_oficial/session/ControleDeSessao.java
@@ -0,0 +1,10 @@
+package com.senai.usuario_database_oficial.session;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class ControleDeSessao {
+
+    public static void registrar (HttpServletRequest httpServletRequest) {
+
+    }
+}
