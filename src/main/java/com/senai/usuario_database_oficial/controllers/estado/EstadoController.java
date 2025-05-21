6411c03f5ae5466eb69b775e0192322dede97efb
CAMILA ALVES <alvescamila87@gmail.com>
2025-05-21 20:41:50 -0300

    feat: delete controller

--- src/main/java/com/senai/usuario_database_oficial/controllers/estado/EstadoController.java
+++ src/main/java/com/senai/usuario_database_oficial/controllers/estado/EstadoController.java
@@ -3,6 +3,7 @@ package com.senai.usuario_database_oficial.controllers.estado;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EstadoController {
 
     @DeleteMapping("{/id}")
-    public ResponseEntity<Void> deletarEstado(Long id) {
+    public ResponseEntity<Void> deletarEstado(@PathVariable Long id) {
         return ResponseEntity.noContent().build();
     }
 }
