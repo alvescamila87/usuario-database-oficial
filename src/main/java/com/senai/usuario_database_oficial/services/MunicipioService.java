ce709dcee8f66b29701cd6dda91dcafb073aa9c0
Camila <alvescamila87@gmail.com>
2025-06-02 20:06:15 -0300

    feat: municipio service

--- src/main/java/com/senai/usuario_database_oficial/services/MunicipioService.java
+++ src/main/java/com/senai/usuario_database_oficial/services/MunicipioService.java
@@ -2,6 +2,7 @@ package com.senai.usuario_database_oficial.services;
 
 import com.senai.usuario_database_oficial.dtos.municipio.MunicipioListaDTO;
 import com.senai.usuario_database_oficial.models.MunicipioModel;
+import com.senai.usuario_database_oficial.repositories.EstadoRepository;
 import com.senai.usuario_database_oficial.repositories.MunicipioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,13 +16,22 @@ public class MunicipioService {
     @Autowired
     MunicipioRepository repository;
 
+    @Autowired
+    EstadoRepository estadoRepository;
+
     public List<MunicipioListaDTO> listarMunicipior() {
         List<MunicipioListaDTO> listaMunicipiosDTO = new ArrayList<>();
 
         List<MunicipioModel> listaMunicipiosModel = repository.findAll();
 
+        for(MunicipioModel municipioModel : listaMunicipiosModel) {
+            MunicipioListaDTO municipioListaDTO = new MunicipioListaDTO();
+            municipioListaDTO.setId(municipioModel.getId());
+            municipioListaDTO.setUf(municipioModel.getEstado().getSigla());
+            municipioListaDTO.setNome(municipioModel.getNome());
 
-        // continuar lógica
+            listaMunicipiosDTO.add(municipioListaDTO);
+        }
 
         return listaMunicipiosDTO;
     }
