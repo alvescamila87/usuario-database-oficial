fc993ada85e104e93e63236f93c78294bced7b40
Camila <alvescamila87@gmail.com>
2025-06-13 20:50:03 -0300

    feat: delete municipio

---
+++ src/main/java/com/senai/usuario_database_oficial/controllers/municipio/MunicipioController.java
@@ -0,0 +1,25 @@
+package com.senai.usuario_database_oficial.controllers.municipio;
+
+import com.senai.usuario_database_oficial.services.MunicipioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class MunicipioController {
+
+    @Autowired
+    MunicipioService service;
+
+    @DeleteMapping("{/id}")
+    public ResponseEntity<Void> deleteMunicipio(@PathVariable Long id) {
+
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
