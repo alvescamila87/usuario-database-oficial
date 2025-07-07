3d0cb759b18e4c7d49d6792be2c102f8babfadc7
Camila <alvescamila87@gmail.com>
2025-07-07 19:40:23 -0300

    feat: controller estado update

--- src/main/java/com/senai/usuario_database_oficial/controllers/estado/AtualizarCadastroEstadoController.java
+++ src/main/java/com/senai/usuario_database_oficial/controllers/estado/AtualizarCadastroEstadoController.java
@@ -1,14 +1,9 @@
 package com.senai.usuario_database_oficial.controllers.estado;
 
-import com.senai.usuario_database_oficial.dtos.categoria.CategoriaDTO;
-import com.senai.usuario_database_oficial.dtos.categoria.CategoriaRequestDTO;
 import com.senai.usuario_database_oficial.dtos.estado.EstadoDTO;
 import com.senai.usuario_database_oficial.dtos.estado.EstadoRequestDTO;
-import com.senai.usuario_database_oficial.dtos.usuario.UsuarioSessaoDTO;
 import com.senai.usuario_database_oficial.exceptions.InvalidOperationException;
-import com.senai.usuario_database_oficial.services.CategoriaService;
 import com.senai.usuario_database_oficial.services.EstadoService;
-import com.senai.usuario_database_oficial.session.ControleDeSessao;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -24,12 +19,12 @@ public class AtualizarCadastroEstadoController {
     EstadoService service;
 
     @GetMapping("/{id}")
-    public  String obterAtualizarCategoria(@PathVariable Long id, Model model, HttpServletRequest httpServletRequest) {
+    public  String obterAtualizarEstado(@PathVariable Long id, Model model, HttpServletRequest httpServletRequest) {
 
-        UsuarioSessaoDTO usuarioSessaoDTO = ControleDeSessao.obter(httpServletRequest);
-        if(usuarioSessaoDTO.getId() == 0L) {
-            return "redirect:/login";
-        }
+//        UsuarioSessaoDTO usuarioSessaoDTO = ControleDeSessao.obter(httpServletRequest);
+//        if(usuarioSessaoDTO.getId() == 0L) {
+//            return "redirect:/login";
+//        }
 
         EstadoDTO atualizarEstadoDTO = service.obterEstadoPorId(id);
         model.addAttribute("atualizarEstadoDTO", atualizarEstadoDTO);
